302ecc8cd567e0320d3972acdf00e7982724ff50
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2011-08-06 16:10:34 +0200

    Use more unique separator.

--- lib/git_handsome.rb
+++ lib/git_handsome.rb
@@ -4,9 +4,9 @@ require 'json'
 
 module GitHandsome
   def self.log
-    json_string = '[' + `git log --format=format:'{@@commit@@:@@%H@@,@@author@@:@@%an@@,@@email@@:@@%ae@@,@@date@@:@@%ai@@,@@subject@@:@@%s@@},'`.chop + ']'
+    json_string = '[' + `git log --format=format:'{!!00!!commit!!00!!:!!00!!%H!!00!!,!!00!!author!!00!!:!!00!!%an!!00!!,!!00!!email!!00!!:!!00!!%ae!!00!!,!!00!!date!!00!!:!!00!!%ai!!00!!,!!00!!subject!!00!!:!!00!!%s!!00!!},'`.chop + ']'
     json_string.gsub! '"', '\\"'
-    json_string.gsub! '@@', '"'
+    json_string.gsub! '!!00!!', '"'
 
     JSON.parse json_string
   end
